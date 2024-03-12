6d65f3753886ee314d6effed11561a25c3aa2687
Polvere243 <danielercoli@gmail.com>
2024-03-12 22:56:07 +0100

    created main structure in the view and imported vite resources

---
+++ resources/views/home.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <!-- importo le risorse di vite -->
+    @vite('rescources/js/app.js')
+</head>
+<body>
+    <section id="movies">
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    <div class="card">
+                        <h2></h2>
+                        <h3></h3>
+                        <h3></h3>
+                        <h3></h3>
+                        <h3></h3>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</body>
+</html>
