15f72b55e44ee56a59978b9d6905991dccf0b87c
Polvere243 <danielercoli@gmail.com>
2024-03-12 22:45:46 +0100

    forgot to import the model

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1,16 +1,17 @@
 <?php
 
-namespace App\Http\Controllers\Controllers;
+namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-
+use App\Models\Movie;
 class HomeController extends Controller
 {
     // single action controller
     public function __invoke()
     {
         $movies = Movie::all();
+        
         return view('home', compact('movies'));
     }
 }
