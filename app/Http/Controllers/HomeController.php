0bd8408d94b233c772ac81532320cefb5dca0529
Polvere243 <danielercoli@gmail.com>
2024-03-12 08:55:45 +0100

    did something completely useless, losing a bit of time

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1,10 +1,11 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Controllers;
 
+use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
-class HOmeController extends Controller
+class HomeController extends Controller
 {
     //
 }
