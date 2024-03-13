5844d5446f9279c414816c0edb3df9c4f575a35d
Polvere243 <danielercoli@gmail.com>
2024-03-13 01:27:43 +0100

    completed exercise

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,32 +4,29 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    
     <!-- importo le risorse di vite -->
-    @vite('rescources/js/app.js')
+    @vite('resources/js/app.js')
 
-    <style>
-        body{
-            display: none;
-        }
-    </style>
+    
 </head>
 <body>
     <section id="movies">
         <div class="container">
             <div class="row">
+                @forelse($movies as $movie)
                 <div class="col">
-                    @forelse($movies as $movie)
                     <div class="card">
-                        <h2></h2>
-                        <h3></h3>
-                        <h3></h3>
-                        <h3></h3>
-                        <h3></h3>
+                        <h2>{{ $movie['title'] }}</h2>
+                        <h3>{{ $movie['original_title'] }}</h3>
+                        <h3>{{ $movie['nationality'] }}</h3>
+                        <h3>{{ $movie['date'] }}</h3>
+                        <h3>{{ $movie['vote'] }}</h3>
                     </div>
+                </div>
                     @empty
                     <h1>Non ci sono film</h1>
                     @endforelse
-                </div>
             </div>
         </div>
     </section>
